7b58d8dcd708a98b0901088796202810f8029c31
GnomoP <ks1202@protonmail.ch>
2018-06-01 00:16:48 -0300

    Few fixes

--- bot.rb
+++ bot.rb
@@ -56,25 +56,34 @@ class CommandBot < Discordrb::Commands::CommandBot
 
   end
 
+  def remove_quote index = 0
+  end
+
   def log_event event
-    server = event.server
     author = event.author
     channel = event.channel
     message = event.message
+
     time = Time.now
     name = "#{author.name}##{author.discriminator}"
 
     out = [
       "[#{time}]:",
-      "    Author:  #{name} (#{author.id})",
-      "    Server:  #{server.name} (#{server.id})",
-      "    Channel: #{channel.name} (#{channel.id})",
       "    Message: #{message.id}",
       "    Content: #{message.content}",]
+
+    if channel.private?
+      out.insert(1, "    Sender:  #{name} (#{author.id})")
+    else
+      server = event.server
+      out.insert(1, "    Author:  #{name} (#{author.id})")
+      out.insert(2, "    Server:  #{server.name} (#{server.id})")
+      out.insert(2, "    Channel: #{channel.name} (#{channel.id})")
+    end
+
     File.open("./requests.log", "a+") do |f|
       f.puts out.join("\n")
     end
-    puts "\nSuccessfuly replied to #{name} (#{server.id})"
   end
 
 end
@@ -82,14 +91,15 @@ end
 bot = CommandBot.new token: OPT["bot_token"], prefix: OPT["prefix"], client_id: OPT["client_id"]
 
 bot.command :add do |event, *text|
-  if event.channel.private?
+  bot.log_event(event)
+
+  if event.channel.private? and event.author.id != OPT["bot_owner"]
     next "Muleque espertinho... (Sua frase **não** foi adicionada)"
   end
 
   text = text.join(' ')
   begin
     bot.add_quote(text)
-    bot.log_event(event)
   rescue Exception => e
     puts e.backtrace.join ""
     puts e.message
@@ -98,41 +108,52 @@ bot.command :add do |event, *text|
   end
 end
 
+bot.command :say do |event, *text|
+  break unless event.user.id == OPT["bot_owner"]
+  bot.log_event(event)
+  event.message.delete() rescue nil
+  text.join(' ')
+end
+
 bot.command :ping do |event|
   mention = event.author.mention
   time = (Time.now - event.timestamp).round(6) * 1000
 
-  bot.log_event(event) unless event.channel.private?
+  bot.log_event(event)
   "#{mention} Criança, não me enche o saco (#{time.round(3)}ms)"
 end
 
 bot.command :source do |event|
-  bot.log_event(event) unless event.channel.private?
+  bot.log_event(event)
   OPT["source_repo"]
 end
 
 bot.command :invite do |event|
-  bot.log_event(event) unless event.channel.private?
+  bot.log_event(event)
   event.bot.invite_url
 end
 
 bot.command :restart do |event|
   break unless event.user.id == OPT["bot_owner"]
-  bot.log_event(event) unless event.channel.private?
-  event.message.react(BYE)
+  bot.log_event(event)
+  event.message.react(BYE) rescue nil
   exit 0
 end
 
 bot.command :quit do |event|
   break unless event.user.id == OPT["bot_owner"]
-  bot.log_event(event) unless event.channel.private?
-  event.message.react(BYE)
+  bot.log_event(event)
+  event.message.react(BYE) rescue nil
   exit 1
 end
 
 bot.run :async
 
-bot.message(contains: bot.bot_user.mention, private: false) do |event|
+bot.ready() do |event|
+  bot.update_status("online", { :game => OPT["game_status"] }, 0, false)
+end
+
+bot.mention() do |event|
   begin
     event.respond(bot.pick_quote)
   rescue Exception => e
@@ -142,6 +163,9 @@ bot.message(contains: bot.bot_user.mention, private: false) do |event|
     bot.log_event(event)
   end
 end
+
+bot.private_message() { |event| bot.log_event(event) }
+bot.message(from: bot.profile.id) { |event| bot.log_event(event) }
 
 shell = Shell.new(bot, OPT)
 shell.loop()
