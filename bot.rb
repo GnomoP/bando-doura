604b2cb0b9289d6a4d63b35d363688910af51dc3
GnomoP <ks1202@protonmail.ch>
2018-06-02 15:13:53 -0300

    Added custom prefixes and proper help command descriptions to commands

--- bot.rb
+++ bot.rb
@@ -4,23 +4,28 @@ require 'discordrb'
 require 'json'
 require './shell'
 
-OK = "\U1F44C".freeze
-BYE = "\U1F44B".freeze
+OK = "\U1F44C"
+BYE = "\U1F44B"
 
-if ARGV[0]
-  fp = ARGV[0]
-else
-  fp = "./config.json"
-end
+NO = "\u274E".freeze
+YES = "\u2705".freeze
 
-File.open(fp, "r+") do |f|
-  if f.read.empty?
-    default = ['{', '  "client_id": "",', '  "bot_token": ""', '}']
-    f.write(default.join "\n")
-  end
+File.open(ARGV[0] || "./config.json", "r+") do |f|
+  f.write("{\n  \n}") if f.read.empty?
 
   f.seek 0
-  OPT = JSON.parse(f.read)
+  CONFIG = JSON.parse(f.read)
+end
+
+File.open("./help.json", "r+") do |f|
+  f.write("{\n  \n}") if f.read.empty?
+
+  f.seek 0
+  HELP = JSON.parse(f.read).map do |k1, v1|
+    k1 = k1.to_sym
+    v1 = v1.map { |k2, v2| [k2.to_sym, v2] }.to_h
+    [k1, v1]
+  end.to_h
 end
 
 class CommandBot < Discordrb::Commands::CommandBot
@@ -73,7 +78,7 @@ class CommandBot < Discordrb::Commands::CommandBot
 
     if channel.private?
 
-      if author.id == bot.profile.id
+      if author.id == CONFIG["client_id"]
         author = event.channel.recipient
         name = "#{author.name}##{author.discriminator}"
         out.insert(1, "  Recipient: #{name} (#{author.id})")
@@ -83,7 +88,7 @@ class CommandBot < Discordrb::Commands::CommandBot
 
     elsif channel.group?
 
-      if author.id == bot.profile.id
+      if author.id == CONFIG["client_id"]
         out.insert(1, "      Group: #{channel.name} (#{channel.id})")
       else
         out.insert(1, "     Author: #{name} (#{author.id})")
@@ -104,14 +109,64 @@ class CommandBot < Discordrb::Commands::CommandBot
 
   end
 
+  def add_prefix id, prefix
+
+    begin
+      prefixes = File.read("./prefixes.json")
+      prefixes = JSON.parse(prefixes)
+
+      prefixes[id.to_s] = prefix
+
+      File.open("./prefixes.json", "w") do |f|
+        f.puts(JSON.pretty_generate(prefixes))
+      end
+    rescue Exception => e
+      puts e.backtrace.join ""
+      puts e.message
+    end
+
+  end
+
+  def get_prefixes
+
+    File.open("./prefixes.json", "r+") do |f|
+      f.write("{\n  \n}") if f.read.empty?
+
+      f.seek 0
+      JSON.parse(f.read)
+    end
+
+  end
+
 end
 
-bot = CommandBot.new token: OPT["bot_token"], prefix: OPT["prefix"], client_id: OPT["client_id"], parse_self: true
+prefix_proc = proc do |message|
+  prefixes = File.open("./prefixes.json", "r+") do |f|
+    f.write("{\n  \n}") if f.read.empty?
 
-bot.command :add, description: "Adiciona uma frase ao dicionário" do |event, *text|
+    f.seek 0
+    JSON.parse(f.read)
+  end
+
+  id = message.channel.server.id rescue message.channel.id
+  prefix = prefixes[id.to_s]  || CONFIG["prefix"]
+
+  message.content[prefix.size .. -1] if message.content.start_with? prefix
+end
+
+OPT = {
+  :token => CONFIG["bot_token"],
+  :client_id => CONFIG["client_id"],
+  :prefix => prefix_proc,
+  :parse_self => true
+}
+
+bot = CommandBot.new OPT
+
+bot.command :add, HELP[:add] do |event, *text|
   bot.log_event(event) unless event.channel.private?
 
-  if event.channel.private? and event.author.id != OPT["bot_owner"]
+  if event.channel.private? and event.author.id != CONFIG["bot_owner"]
     next "Muleque espertinho... (Sua frase **não** foi adicionada)"
   end
 
@@ -126,14 +181,14 @@ bot.command :add, description: "Adiciona uma frase ao dicionário" do |event, *t
   end
 end
 
-bot.command :say, description: "**[OWNER ONLY]** Manda uma mensagem" do |event, *text|
-  break unless event.user.id == OPT["bot_owner"]
+bot.command :say, HELP[:say] do |event, *text|
+  break unless event.user.id == CONFIG["bot_owner"]
   bot.log_event(event) unless event.channel.private?
   event.message.delete() rescue puts "Couldn't delete message"
   text.join(' ')
 end
 
-bot.command :ping, description: "Checa o ping do bot, em milisegundos" do |event|
+bot.command :ping, HELP[:ping] do |event|
   mention = event.author.mention
   time = (Time.now - event.timestamp).round(6) * 1000
 
@@ -141,25 +196,45 @@ bot.command :ping, description: "Checa o ping do bot, em milisegundos" do |event
   "#{mention} Criança, não me enche o saco (#{time.round(3)}ms)"
 end
 
-bot.command :source, description: "Manda o link pro repositório do Bot." do |event|
-  bot.log_event(event) unless event.channel.private?
-  OPT["source_repo"]
+bot.command :prefix, HELP[:prefix] do |event, *text|
+  text = text.join(' ')
+
+  if event.server
+    permission = event.user.permission?(:administrator, event.channel)
+    unless permission || event.user.id == CONFIG["bot_owner"]
+      next "Apenas administradores podem mudar o prefixo do servidor."
+    end
+  end
+
+  id = event.server.id rescue event.channel.id
+  prefix = bot.get_prefixes()[id]
+
+  prefix = CONFIG["prefix"] unless prefix
+  next "Prefixo atual: \"#{prefix}\". Use `#{prefix}prefix Novo Prefixo` para alterá-lo." if text.empty?
+
+  bot.add_prefix(id, text)
+  "Prefixo alterado: de \"#{prefix}\" para \"#{text}\""
 end
 
-bot.command :invite, description: "Manda o link para convidar o Bot a um servidor" do |event|
+bot.command :source, HELP[:source] do |event|
+  bot.log_event(event) unless event.channel.private?
+  CONFIG["source_repo"]
+end
+
+bot.command :invite, HELP[:invite] do |event|
   bot.log_event(event) unless event.channel.private?
   event.bot.invite_url
 end
 
-bot.command :restart, description: "**[OWNER ONLY]** Reinicia o Bot" do |event|
-  break unless event.user.id == OPT["bot_owner"]
+bot.command :restart, HELP[:restart] do |event|
+  break unless event.user.id == CONFIG["bot_owner"]
   bot.log_event(event) unless event.channel.private?
   event.message.react(BYE) rescue nil
   exit 0
 end
 
-bot.command :quit, description: "**[OWNER ONLY]** Desliga o Bot" do |event|
-  break unless event.user.id == OPT["bot_owner"]
+bot.command :quit, HELP[:quit] do |event|
+  break unless event.user.id == CONFIG["bot_owner"]
   bot.log_event(event) unless event.channel.private?
   event.message.react(BYE) rescue nil
   exit 1
@@ -167,9 +242,16 @@ end
 
 bot.run :async
 
-bot.mention() do |event|
+bot.ready do |event|
+  sleep(10) and bot.game = CONFIG["game_status"]
+end
+
+bot.mention contains: /prefixo?/i do |event|
   begin
-    event.respond(bot.pick_quote)
+    id = event.server.id rescue event.channel.id
+    prefix = bot.get_prefixes()[id] || "-"
+
+    event << "Prefixo atual: \"#{prefix}\". Use `#{prefix}prefix Novo Prefixo` para alterá-lo."
   rescue Exception => e
     puts e.backtrace.join ""
     puts e.message
@@ -178,9 +260,24 @@ bot.mention() do |event|
   end
 end
 
-bot.private_message() { |event| bot.log_event(event) }
-bot.message(from: bot.profile, private: false) { |event| bot.log_event(event) }
-bot.ready() { |event| bot.game = OPT["game_status"] }
+bot.mention contains: not!(/prefixo?/i) do |event|
+  begin
+    event << bot.pick_quote
+  rescue Exception => e
+    puts e.backtrace.join ""
+    puts e.message
+  ensure
+    bot.log_event(event) unless event.channel.private?
+  end
+end
 
-shell = Shell.new(bot, OPT)
+bot.private_message do |event|
+  bot.log_event(event)
+end
+
+bot.message from: bot.profile, private: false do |event|
+  bot.log_event(event)
+end
+
+shell = Shell.new(bot, CONFIG)
 shell.loop()
